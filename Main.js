1eb4448f3cb894380c3794c2364f372dc0312fa0
Tomasz Borek <Tomasz.Borek@lumesse.com>
2013-04-16 02:50:50 +0200

    Main added with Constructor.
    * constructor takes sound source and Options
    * Options === Settings, I just chose a different name
    * `opt || new Opt` is to make it easier: to test and for browsers
    * code verified with online JSLint (vide header)
    
    Questions:
    * default options - to be decided
    * would be nice to plug in tests
    * do we use strict? if it's a partially tutorial project - I guess so?)

---
+++ Main.js
@@ -0,0 +1,14 @@
+/*properties
+    insert
+*/
+
+function webEq(soundSrc, options) {
+		options = options || new Options();
+		options.insert('sound-source', soundSrc);
+}
+
+
+var Options = Options || (function() {
+			// here will all Options be set as by default they are to be set
+			// but for now I have no clue what would go here		
+}
