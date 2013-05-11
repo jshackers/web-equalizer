7baa04481dd858d18c24bc25714e8cec0fee070c
Krzysztof Safjanowski <krzysztof@safjanowski.pl>
2013-05-11 14:38:50 +0200

    read values from inputs

--- app/engine/script.js
+++ app/engine/script.js
@@ -1,1 +1,20 @@
-console.log('test');
+var eq = (function equalizer (argument) {
+
+  var eqContainer = document.querySelector('#eq');
+  var inputs = eqContainer.querySelectorAll('input[type="range"]');
+
+  eqContainer.addEventListener('submit', formSubmitHandler, false);
+
+  function formSubmitHandler (event) {
+    event.preventDefault();
+    var controls = [];
+
+    Array.prototype.map.call(inputs, function(element, index) {
+      return controls[index] = element.value;
+    });
+
+    console.log(controls);
+    
+    return controls;
+  }
+}());
