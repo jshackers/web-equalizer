6618c7aff3f4e9d64d2bc23de4e32b3bd7ba6ec0
Naugtur <naugtur@gmail.com>
2013-05-11 13:23:43 +0200

    some new methods scaffolding

--- app/main.js
+++ app/main.js
@@ -4,18 +4,31 @@ var webEq = (function(){
 //change it to require a file from engine
 var nSplitter = (function(){
 
-	var source,
+	var source,bands=[],
 	context = new webkitAudioContext();
 
 	function setSource(audio){
 		source = context.createMediaElementSource(audio);
-		
+
 	}
 
+	function splitTo(n){
+		//split sound to n Bands
+
+		//save each band in bands array
+	}
+
+	function getBand(num){
+		return band[num];
+	} 
+
 	return{
-		setSource:setSource
+		setSource:setSource,
+		splitTo:splitTo,
+		getBand:getBand
 	}
 })();
+
 
 return function(node){
 
